49aceee08cabb62b716b1031a72eab99f5db0d15
stevenlai1688 <stevenlai1688@gmail.com>
2022-05-24 18:33:54 -0500

    changed fields to match the rest of the model

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 "use strict";
 exports.__esModule = true;
+exports.CREDENTIAL_TYPE_ENUM = void 0;
 var fs = require("fs");
 var csv_parse_1 = require("csv-parse");
 var path = require("path");
+//import csv from 'csv-parser';
+var CREDENTIAL_TYPE_ENUM;
+(function (CREDENTIAL_TYPE_ENUM) {
+    CREDENTIAL_TYPE_ENUM["hidproxfob"] = "hidproxfob";
+    CREDENTIAL_TYPE_ENUM["android"] = "android";
+    CREDENTIAL_TYPE_ENUM["ios"] = "ios";
+    CREDENTIAL_TYPE_ENUM["uwbfob"] = "uwbfob";
+    CREDENTIAL_TYPE_ENUM["iclass"] = "iclass";
+    CREDENTIAL_TYPE_ENUM["tx5577"] = "tx5577";
+})(CREDENTIAL_TYPE_ENUM = exports.CREDENTIAL_TYPE_ENUM || (exports.CREDENTIAL_TYPE_ENUM = {}));
 // find absolute path
 var csvFile = path.resolve(__dirname, 'Users.csv');
 // each header, allow to skip first line of csv file
 var headers = ['_id', 'keys', 'firstName', 'surname', 'email', 'phone', 'memberStatus', 'profileImage', 'lastLoginAt', 'issuer', '__v'];
 var output = [];
+// another potential way to approach this
 // create a read stream
 //https://www.npmjs.com/package/csv-parser
 // const file = fs.createReadStream(csvFile, 'utf-8')
@@ -19,23 +31,38 @@ var output = [];
 //                 console.log(result);
 //             });
 // https://blog.tericcabrel.com/read-csv-node-typescript/
+// https://csv.js.org/parse/options/
 // read from file
 var file = fs.readFileSync(csvFile, 'utf-8');
 // parse from file using delimiter, specifies headers and start from line 2, with externally casting
-(0, csv_parse_1.parse)(file, { delimiter: ',', columns: headers, fromLine: 2, cast: function (value, context) {
-        // parse phone number to integer
+(0, csv_parse_1.parse)(file, { delimiter: ',', columns: headers, fromLine: 2,
+    cast: function (value, context) {
+        // parse phone number to integer, if field is empty, then return 0
         if (context.column === 'phone') {
             return parseInt(value) ? parseInt(value) : 0;
         }
+        else if (context.column === 'lastLoginAt') {
+            return parseInt(value);
+        }
+        else if (context.column === '__v') {
+            return parseInt(value);
+        }
+        // parse the keys, so far only splitting on semi-colon
+        else if (context.column === 'keys') {
+            var key = value.split(';');
+            return key;
+        }
         return value;
-    } }, function (error, user) {
+    }
+}, 
+// callback
+function (error, user) {
     if (error) {
         console.error(error);
     }
-    // modify each field value of the user
+    // can manually modify each field value of the user
     user.forEach(function (user) {
-        user.__v = user.__v.valueOf();
-        user.firstName = user.firstName.toUpperCase();
+        //user.firstName = user.firstName.toUpperCase();
         user.memberStatus = (user.memberStatus.toString().toLowerCase() === 'true');
     });
     // output
--- index.ts
+++ index.ts
@@ -2,11 +2,19 @@ import * as fs from 'fs';
 import {parse} from 'csv-parse';
 import * as path from 'path';
 //import csv from 'csv-parser';
+export enum CREDENTIAL_TYPE_ENUM {
+    hidproxfob = 'hidproxfob',
+    android = 'android',
+    ios = 'ios',
+    uwbfob = 'uwbfob',
+    iclass = 'iclass',
+    tx5577 = 'tx5577'
+}
 
 interface key {
     name: string; // Human friendly name
     key: string // The actual key
-    type: Enumerator; // The type of credential
+    type: CREDENTIAL_TYPE_ENUM; // The type of credential
 }
 
 type User = {
@@ -28,6 +36,7 @@ const csvFile = path.resolve(__dirname, 'Users.csv');
 // each header, allow to skip first line of csv file
 const headers = ['_id', 'keys', 'firstName', 'surname', 'email', 'phone', 'memberStatus', 'profileImage', 'lastLoginAt', 'issuer', '__v'];
 const output = [];
+// another potential way to approach this
 // create a read stream
 //https://www.npmjs.com/package/csv-parser
 // const file = fs.createReadStream(csvFile, 'utf-8')
@@ -40,17 +49,26 @@ const output = [];
 //             });
 
 // https://blog.tericcabrel.com/read-csv-node-typescript/
+// https://csv.js.org/parse/options/
 // read from file
 const file = fs.readFileSync(csvFile, 'utf-8');
 // parse from file using delimiter, specifies headers and start from line 2, with externally casting
 parse(file, { delimiter: ',', columns: headers, fromLine: 2, 
     cast: (value, context) => {
         // parse phone number to integer, if field is empty, then return 0
-        if (context.column === 'phone'){
-            return parseInt(value) ? parseInt(value) : 0;
+        if (context.column === 'lastLoginAt'){
+            return parseInt(value);
+        }
+        else if (context.column === '__v'){
+            return parseInt(value);
+        }
+        // parse the keys, so far only splitting on semi-colon (depends on implementations)
+        else if (context.column === 'keys'){
+            const key = value.split(';');
+            return key;
         }
         return value;
-    }
+    },
 },
 // callback
 (error, user: User[]) => {
@@ -59,8 +77,7 @@ parse(file, { delimiter: ',', columns: headers, fromLine: 2,
         }
         // can manually modify each field value of the user
         user.forEach(user => {
-            user.__v = user.__v.valueOf();
-            user.firstName = user.firstName.toUpperCase();
+            //user.firstName = user.firstName.toUpperCase();
             user.memberStatus = (user.memberStatus.toString().toLowerCase() === 'true');
         });
         // output
