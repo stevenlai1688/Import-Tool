d19694603e20de5daee534e8930ba3e1d493d2ca
stevenlai1688 <stevenlai1688@gmail.com>
2022-05-25 17:56:29 -0500

    bug need to be fixed, try to fix keys.

--- User.js
+++ User.js
@@ -14,8 +14,18 @@ var CREDENTIAL_TYPE_ENUM;
     CREDENTIAL_TYPE_ENUM["iclass"] = "iclass";
     CREDENTIAL_TYPE_ENUM["tx5577"] = "tx5577";
 })(CREDENTIAL_TYPE_ENUM = exports.CREDENTIAL_TYPE_ENUM || (exports.CREDENTIAL_TYPE_ENUM = {}));
+// function fillKeys (str: string, userList: UserList[]){
+//   for(const user of userList){
+// for (const key of user.keys){
+//   key.name = str.split(',')[0].trim();
+//   key.key = str.split(',')[1].trim();
+//   key.type = str.split(',')[2].trim();
+// }
+//   }
+// }
 // find absolute path
 var csvFile = path.resolve(__dirname, 'Users.csv');
+console.log(csvFile);
 // each header, allow to skip first line of csv file
 var headers = ['keys', 'firstName', 'surname', 'email', 'phone', 'memberStatus'];
 // --- another potential way to approach this
@@ -34,6 +44,11 @@ var headers = ['keys', 'firstName', 'surname', 'email', 'phone', 'memberStatus']
 // https://csv.js.org/parse/options/
 // read from file
 var file = fs.readFileSync(csvFile, 'utf-8');
+var userList = [];
+var keyList = [];
+var typeList = [];
+var nameList = [];
+var keyToAdd = [];
 // parse from file using delimiter, specifies headers and start from line 2, with external casting
 (0, csv_parse_1.parse)(file, {
     delimiter: ',',
@@ -42,7 +57,18 @@ var file = fs.readFileSync(csvFile, 'utf-8');
     cast: function (value, context) {
         // parse the keys, so far only splitting on semi-colon
         if (context.column === 'keys') {
-            var output = value.split(';');
+            var output = value.split(';').map(function (element) { return element.trim(); });
+            for (var i = 0; i < output.length; i++) {
+                // console.log(output[i] + "IS OUTPUT "+i);
+                nameList.push(output[i].split(',')[0]);
+                keyList.push(output[i].split(',')[1]);
+                typeList.push(output[i].split(',')[2]);
+                // BUG FIX HERE!!!, need to make keyToAdd a 2D array, push two values in the first row, push 0 values in 2nd row
+                keyToAdd.push({ name: nameList[i].trim(), key: keyList[i].trim(), type: typeList[i].trim() });
+                // console.log(nameList);
+                // console.log(typeList);
+                // console.log(keyList);
+            }
             return output;
         }
         // return boolean value based on string
@@ -57,6 +83,28 @@ function (error, user) {
     if (error) {
         console.error(error);
     }
+    // add from each user to a list
+    for (var i = 0; i < user.length; i++) {
+        userList.push({
+            // if key does not exist, then it is an empty string
+            // BUG TO FIX
+            // keytoadd at index 0 (user number) should contain two key arrays, at index 1 should contain 0 key arrays
+            keys: keyToAdd[i],
+            firstName: user[i].firstName,
+            surname: user[i].surname,
+            email: user[i].email,
+            phone: user[i].phone,
+            memberStatus: user[i].memberStatus
+        });
+    }
     // output
-    console.log(user);
+    for (var i = 0; i < userList.length; i++) {
+        for (var _i = 0, _a = userList[i].keys; _i < _a.length; _i++) {
+            var key = _a[_i];
+            console.log(key.name);
+            console.log(key.key);
+            console.log(key.type);
+        }
+    }
+    console.log(userList);
 });
--- User.ts
+++ User.ts
@@ -12,23 +12,40 @@ export enum CREDENTIAL_TYPE_ENUM {
     tx5577 = 'tx5577'
 }
 
-interface key {
-    name: string; // Human friendly name
-    key: string // The actual key
-    type: CREDENTIAL_TYPE_ENUM; // The type of credential
+// type User = {
+//   keys: {
+//     name: string, // Human friendly name
+//     key: string, // The actual key
+//     type: CREDENTIAL_TYPE_ENUM}[]// The type of credential
+//   firstName: string;
+//   surname: string;
+//   email: string;
+//   phone: string;
+//   memberStatus: boolean;
+// };
+export interface UserList {
+  keys: {
+      name: string, // Human friendly name
+      key: string, // The actual key
+      type: string}[]// The type of credential
+  firstName: string;
+  surname: string;
+  email: string;
+  phone: string;
+  memberStatus: boolean;
 }
-
-type User = {
-    keys: key[];
-    firstName: string;
-    surname: string;
-    email: string;
-    phone: string;
-    memberStatus: boolean;
-};
-
+// function fillKeys (str: string, userList: UserList[]){
+//   for(const user of userList){
+    // for (const key of user.keys){
+    //   key.name = str.split(',')[0].trim();
+    //   key.key = str.split(',')[1].trim();
+    //   key.type = str.split(',')[2].trim();
+    // }
+//   }
+// }
 // find absolute path
 const csvFile = path.resolve(__dirname, 'Users.csv');
+console.log(csvFile);
 // each header, allow to skip first line of csv file
 const headers = ['keys', 'firstName', 'surname', 'email', 'phone', 'memberStatus'];
 // --- another potential way to approach this
@@ -48,6 +65,11 @@ const headers = ['keys', 'firstName', 'surname', 'email', 'phone', 'memberStatus
 // https://csv.js.org/parse/options/
 // read from file
 const file = fs.readFileSync(csvFile, 'utf-8');
+const userList: UserList[] = [];
+const keyList:string[] = [];
+const typeList:string[] = [];
+const nameList:string[] = [];
+const keyToAdd = [];
 // parse from file using delimiter, specifies headers and start from line 2, with external casting
 parse(
   file,
@@ -58,7 +80,21 @@ parse(
     cast: (value, context) => {
       // parse the keys, so far only splitting on semi-colon
       if (context.column === 'keys') {
-        const output = value.split(';').map(element => element.trim());
+        const output: string[] = value.split(';').map(element => element.trim());
+        for (let i = 0; i < output.length; i++){
+          // console.log(output[i] + "IS OUTPUT "+i);
+
+          nameList.push(output[i].split(',')[0]);
+          keyList.push(output[i].split(',')[1]);
+          typeList.push(output[i].split(',')[2]);
+
+          // BUG FIX HERE!!!, need to make keyToAdd a 2D array, push two values in the first row, push 0 values in 2nd row
+          keyToAdd.push ({name: nameList[i].trim(), key: keyList[i].trim(), type: typeList[i].trim()});
+
+          // console.log(nameList);
+          // console.log(typeList);
+          // console.log(keyList);
+        }
         return output;
       }
       // return boolean value based on string
@@ -69,11 +105,34 @@ parse(
     },
   },
   // callback
-  (error, user: User[]) => {
+  (error, user: UserList[]) => {
     if (error) {
       console.error(error);
     }
+    // add from each user to a list
+    for (let i = 0; i < user.length; i++){
+      userList.push({
+        // if key does not exist, then it is an empty string
+        // BUG TO FIX
+        // keytoadd at index 0 (user number) should contain two key arrays, at index 1 should contain 0 key arrays
+        keys: keyToAdd[i],
+        firstName: user[i].firstName,
+        surname: user[i].surname,
+        email: user[i].email,
+        phone: user[i].phone,
+        memberStatus: user[i].memberStatus
+      });
+    }
+    
     // output
-    console.log(user);
-  },
+    for (let i = 0; i < userList.length; i++){
+      for (const key of userList[i].keys){
+        console.log(key.name);
+        console.log(key.key);
+        console.log(key.type);
+      }
+    }
+    console.log(userList);
+  
+  }
 );
